fd5ab86dd2ea3832c11f64f6ca0a318d93388c57
Ewa Utracka <ewczi@Ewas-MacBook-Pro.local>
2019-09-02 17:00:19 +0200

    transition and colors added

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ function makeChart() {
 
 function changeHeight() {
   for (i = 0; i < arr.length; i++) {
+    document.querySelector(".bar" + (i + 1)).classList.remove("hidden");
     document.querySelector(".bar" + (i + 1)).style.height = arr[i] + "rem";
+    document.querySelector(".bar" + (i + 1)).style.transition =
+      "all linear .2s";
+    if (arr[i] > 30) {
+      document.querySelector(".bar" + (i + 1)).style.background = "#FFBFE9";
+    } else if (arr[i] > 20) {
+      document.querySelector(".bar" + (i + 1)).style.background = "#BF8FAF";
+    } else if (arr[i] > 10) {
+      document.querySelector(".bar" + (i + 1)).style.background = "#9C758E";
+    } else {
+      document.querySelector(".bar" + (i + 1)).style.background = "#6B5062";
+    }
   }
 }
 
@@ -29,4 +41,4 @@ function buildArray() {
   console.log(arr);
 }
 
-setInterval(makeChart, 100);
+setInterval(makeChart, 1000);
